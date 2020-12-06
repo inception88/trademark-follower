5a1e2c6b9c035392c89e4f15f6da46632ca5fb76
inception88 <bljones36@gmail.com>
2020-12-06 00:31:59 -0500

    Updated App.js to add Trademark Search component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import Trademark from './components/trademarks/Trademark.js'
+import TrademarkSearch from './components/trademarks/TrademarkSearch'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Trademark />
+      <TrademarkSearch />
     </div>
   );
 }
