e7f9edfc2480b9655ac5ac28a47f898900c0c07a
inception88 <bljones36@gmail.com>
2020-12-06 01:38:12 -0500

    Added TrademarkSearch component

---
+++ src/components/trademarks/TrademarkSearch.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+
+const API_KEY = process.env.REACT_APP_USPTO_API_KEY;
+
+class TrademarkSearch extends Component {
+
+    state = {
+        data: JSON,
+        mark: '',
+        serialNumber: '',
+        status: '',
+        prosecutionHistory: '',
+        statusDate: '',
+        filingDate: '',
+        registrationNumber: ''
+      }
+
+    saveState(JSON){
+        console.log(JSON.trademarks[0].status.markElement)
+        this.setState({
+            data: JSON,
+            mark: JSON.trademarks[0].status.markElement
+            });
+}
+
+trademarkSearch = (sn) => {
+    fetch(`https://cors-anywhere.herokuapp.com/https://tsdrapi.uspto.gov/ts/cd/casestatus/sn${sn}/info`, { headers: {"USPTO-API-KEY": API_KEY}}
+    ).then(response => { 
+        return response.json() 
+    }).then(JSON => { 
+        this.saveState(JSON)
+  })
+}
+//90130409 test Serial number: Ben Jones Codes
+handleOnChange(event) {
+    this.setState({
+      text: event.target.value,
+    });
+  }
+
+  handleOnSubmit(event) {
+    event.preventDefault();
+    this.trademarkSearch(this.state.text);
+  }
+
+render() {
+    return (
+      <div>
+        <form onSubmit={(event) => this.handleOnSubmit(event)}>
+          <input
+            type="text"
+            value={this.state.text}
+            onChange={(event) => this.handleOnChange(event)} />
+          <input type="submit" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default TrademarkSearch
