656624104624e1a6aebf4ff4a64c9ae03b44a499
inception88 <bljones36@gmail.com>
2020-12-05 22:31:55 -0500

    Tested connection to USPTO database and added API key to .env file

--- src/Trademark.js
+++ src/Trademark.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 
+const API_KEY = process.env.REACT_APP_USPTO_API_KEY;
+
 class Trademark extends Component {
 
 trademarks = () => {
-    fetch('http://localhost:3000/api/v1/trademarks.json').then(response => { 
+    fetch('https://cors-anywhere.herokuapp.com/https://tsdrapi.uspto.gov/ts/cd/casestatus/sn90130409/info', { headers: {"USPTO-API-KEY": API_KEY}}
+    ).then(response => { 
         return response.json() 
     }).then(responseJSON => { 
       console.log(responseJSON)
--- src/actions/trademarkActions.js
+++ src/actions/trademarkActions.js
@@ -1,7 +1,7 @@
 export const fetchTrademarks = () => {
     return (dispatch) => {
       dispatch({ type: 'LOADING_TRADEMARKS'})
-      fetch('https://http://localhost:3000/api/v1/trademarks.json').then(response => {
+      fetch('https://tsdrapi.uspto.gov/ts/cd/').then(response => {
         return response.json()
       }).then(responseJSON => {
         dispatch({ type: 'ADD_TRADEMARKS', trademarks: responseJSON })
