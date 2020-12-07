f88b45f8d5d7931f27c392a05ae5220f8f15cf7f
inception88 <bljones36@gmail.com>
2020-12-07 00:02:05 -0500

    Added Navbar.js and added routing to index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Navbar from './components/navigation/Navbar';
+import Error from './components/errors/Error';
+import TrademarkSearch from './components/trademarks/TrademarkSearch';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <Navbar />
+      <Route exact path="/" component={Error} />
+      <Route exact path="/search" component={TrademarkSearch} />
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Router>,
   document.getElementById('root')
 );
 
