902e3d635ef287e3b292321860baee4a8f87bad5
inception88 <bljones36@gmail.com>
2020-12-10 16:39:44 -0500

    Added an example to the home component

--- src/components/navigation/Home.js
+++ src/components/navigation/Home.js
@@ -6,7 +6,9 @@ class Home extends Component {
   render() {
     return (
       <div>
-          <h1>USPTO.GOV Trademark search database: <a target ='_blank' href="http://tmsearch.uspto.gov/bin/gate.exe?f=tess&state=4809:my35km.1.1">Click Here</a></h1>
+          <h1>USPTO.GOV Trademark Search Database: <a target ='_blank' href="http://tmsearch.uspto.gov/bin/gate.exe?f=tess&state=4809:my35km.1.1">Click Here</a></h1>
+          <h1>USPTO.GOV Basic Word Mark Search: <a target ='_blank' href="http://tmsearch.uspto.gov/bin/gate.exe?f=searchss&state=4809:my35km.1.1">Click Here</a></h1>
+          <h2>Trademark Example Serial Number: EnterAction Apparel (87818121)</h2>
             <TrademarkSearch/>
       </div>
     )
