068108b1fbb4e0ce3591ee430a178bb2d9497e0c
inception88 <bljones36@gmail.com>
2020-12-10 16:32:56 -0500

    Moved router and components within App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
-import TrademarkSearch from './components/trademarks/TrademarkSearch'
+import NavBar from './components/navigation/Navbar';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './components/navigation/Home';
+import TrademarkSearch from './components/trademarks/TrademarkSearch';
 
 function App() {
   return (
     <div className="App">
+    <Router>
+      <NavBar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/home" component={TrademarkSearch} />
+      <Route exact path="/search" component={TrademarkSearch} />
+    </Router>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,21 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import NavBar from './components/navigation/Navbar';
-import Home from './components/navigation/Home';
-import TrademarkSearch from './components/trademarks/TrademarkSearch';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <NavBar />
-      <App />
-      <Route exact path="/" component={Home} />
-      <Route exact path="/home" component={TrademarkSearch} />
-      <Route exact path="/search" component={TrademarkSearch} />
-    </Router>
+    < App />
   </React.StrictMode>,
   document.getElementById('root')
 );
