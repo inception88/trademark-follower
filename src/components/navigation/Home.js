57fa20508a209788b17acb7d9108469119b51a4d
inception88 <bljones36@gmail.com>
2020-12-12 10:08:13 -0500

    Swapped bcrypt gem for jwt gem

--- src/components/navigation/Home.js
+++ src/components/navigation/Home.js
@@ -6,7 +6,7 @@ class Home extends Component {
   render() {
     return (
       <div>
-          <h1>USPTO.GOV Trademark Electronic Search System(TESS): <a target ='_blank' href="http://tess2.uspto.gov/">Click Here</a></h1>
+          <h1><a target ='_blank' href="http://tess2.uspto.gov/">Trademark Electronic Search System(TESS)</a></h1>
           <h2>Trademark Example Serial Number: EnterAction Apparel (87818121)</h2>
             <TrademarkSearch/>
       </div>
