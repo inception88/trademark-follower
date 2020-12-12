772b2748f4a7c1a3eef92b39988f3ae9a84bfc92
inception88 <bljones36@gmail.com>
2020-12-12 08:07:48 -0500

    Updated TESS link on Home component

--- src/components/navigation/Home.js
+++ src/components/navigation/Home.js
@@ -6,8 +6,7 @@ class Home extends Component {
   render() {
     return (
       <div>
-          <h1>USPTO.GOV Trademark Search Database: <a target ='_blank' href="http://tmsearch.uspto.gov/bin/gate.exe?f=tess&state=4809:my35km.1.1">Click Here</a></h1>
-          <h1>USPTO.GOV Basic Word Mark Search: <a target ='_blank' href="http://tmsearch.uspto.gov/bin/gate.exe?f=searchss&state=4809:my35km.1.1">Click Here</a></h1>
+          <h1>USPTO.GOV Trademark Electronic Search System(TESS): <a target ='_blank' href="http://tess2.uspto.gov/">Click Here</a></h1>
           <h2>Trademark Example Serial Number: EnterAction Apparel (87818121)</h2>
             <TrademarkSearch/>
       </div>
