ac7f2d43046322bef3d3468a6f410704fdb58cf3
inception88 <bljones36@gmail.com>
2020-12-19 15:02:40 -0500

    Added clickable link for trademarks in my follows

--- src/components/trademarks/Trademarks.js
+++ src/components/trademarks/Trademarks.js
@@ -3,14 +3,20 @@ import Trademark from '../trademarks/Trademark';
 
 
 class Trademarks extends Component {
+
+    fetchTrademark = (trademark) => () => {
+        console.log(trademark)
+    }
+
   render() {
     return(
-      <ul>
+      <div>
         {this.props.trademarks.map((trademark, index) => {
-        return <li><Trademark key={index} trademark={trademark}/></li>})}
-      </ul>
+           return <a onClick={this.fetchTrademark(trademark)}><h1>{index+1}. {trademark.mark}</h1></a>})}
+      </div>
     );
   }
 };
+//return <li><Trademark key={index} trademark={trademark}/></li>})}
 
 export default Trademarks;
